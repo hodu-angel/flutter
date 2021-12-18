9e565568a1fceb5d7e0355f8df1c463cb4c9f283
seohyoju <kinnylovemy@gmail.com>
2021-12-18 18:51:16 +0900

    SizedBox() 와 Container() 의 차이점 추가 및 변경

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -14,13 +14,11 @@ class Basic extends StatelessWidget {
             appBar: AppBar(
               title: Text('앱임'),
             ),
-            body: Container(
-              child: Text('안녕'),
-            ),
+            body: Text('안녕'),
             bottomNavigationBar: BottomAppBar(
-                child: Container(
-                  width: 50,
-                  height: 50,
+                //width, height, child만 필요한 박스는 SizedBox() 를 사용함
+                child: SizedBox(  //Container는 무겁기때문에 SizedBox가 더 가벼워서 Lint가 잡아준다.
+                  height: 70,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
