78cb78f90dc5280bdf8b6e189bbb48a38cc37593
seohyoju <kinnylovemy@gmail.com>
2021-12-20 18:05:24 +0900

    211220-flutter-basic-box.md

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -6,21 +6,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
-        home: Scaffold(
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.star),
-              Icon(Icons.shop),
-            ],
+      home: Scaffold(
+        appBar: AppBar( title: Text('앱임') ),
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: 150, height: 50,
+            //박스 꾸미기
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black)
+            ),
+            //박스 위치 여백주기
+            margin: EdgeInsets.all(30),
+            //박스 내의 여백주기
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            child: Text('padding'),
           ),
-        )
+        ),
+        bottomNavigationBar: BottomAppBar(),
+      ),
     );
   }
 }
-
