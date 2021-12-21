cae0b862395c0c8434ec9c06c048eb818a22428e
seohyoju <kinnylovemy@gmail.com>
2021-12-22 02:56:52 +0900

    Expanded(), flex 비율조정, alignment 조정

--- camera/lib/main.dart
+++ camera/lib/main.dart
@@ -17,14 +17,14 @@ class Camera extends StatelessWidget {
   }
 }
 
-class MainPage extends StatefulWidget{
+class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage>{
+class _MainPageState extends State<MainPage> {
   final _valueList = ['금호동1가', '금호동2가', '금호동3가'];
-  String _value ='금호동3가';
+  String _value = '금호동3가';
 
   @override
   Widget build(BuildContext context) {
@@ -36,14 +36,13 @@ class _MainPageState extends State<MainPage>{
           icon: Icon(Icons.keyboard_arrow_down),
           underline: Container(),
           items: _valueList.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                        child: Text(value),
-                    );
-
-          },).toList(),
-
+            (value) {
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              );
+            },
+          ).toList(),
           onChanged: (value) {
             setState(() {
               _value = value!;
@@ -52,20 +51,80 @@ class _MainPageState extends State<MainPage>{
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.search, color: Colors.black,),
-            onPressed: (){},
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {},
           ),
           IconButton(
-            icon: Icon(Icons.menu, color: Colors.black,),
-            onPressed: (){},
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {},
           ),
           IconButton(
-            icon: Icon(Icons.notifications_none, color: Colors.black,),
-            onPressed: (){},
+            icon: Icon(
+              Icons.notifications_none,
+              color: Colors.black,
+            ),
+            onPressed: () {},
           ),
         ],
       ),
-      body: Text(''),
+      body: Container(
+        height: 150,
+        color: Colors.white,
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Image.asset(
+                'camera.jpeg',
+                width: 150,
+              ),
+              flex: 3,
+            ),
+
+            //우측 상품정보
+            Expanded(
+              child: Container(
+                //폭을 %로 줄 수 있다.
+                width: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      '성동구 청담동 끌올 10분 전',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Text('210,000원'),
+
+                    //글자나 아이콘은 자기 크기만큼의 사이즈를 가지려 한다.
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              flex: 7,
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(),
     );
   }
