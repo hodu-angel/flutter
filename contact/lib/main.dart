88b14f38c0dd18c7f1cd9f37820bf8615fb34abd
seohyoju <kinnylovemy@gmail.com>
2021-12-28 23:08:38 +0900

    Custom Widget

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -4,6 +4,10 @@ void main() {
   runApp(const MyApp());
 }
 
+var a = SizedBox(
+  child: Text('안녕'),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,18 +15,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        //AppBar에는 leading, title, actions 사용가능
-        appBar: AppBar(
-          leading: Icon(Icons.star),
-          title: Text('앱이얌'),
-          actions: [Icon(Icons.star), Icon(Icons.shop)],
-        ),
-        body: SizedBox(
-          //button은 기본적으로 child:와 onPressed:가 있어야 잘 보인다.
-          child: IconButton(
-            icon: Icon(Icons.star),
-            onPressed: (){} ,
-          ),
+        appBar: AppBar(),
+        //Custom Widget
+        body: ListView(
+          children: [
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+          ],
         ),
       ),
     );
