b1046cc9123b674b211d4cb74aab3e34cb5632df
seohyoju <kinnylovemy@gmail.com>
2022-01-17 20:06:22 +0900

    무료나눔error modify, null data 해당지역데이터가없습니다 show

--- carrot_market_sample/lib/page/home.dart
+++ carrot_market_sample/lib/page/home.dart
@@ -34,6 +34,7 @@ class _HomeState extends State<Home> {
   //가격에 '원' 붙이기
   final oCcy = NumberFormat('#,###', 'ko_KR');
   String calcStringToWon(String priceString){
+    if(priceString == '무료나눔') return priceString;
     return "${oCcy.format(int.parse(priceString))}원";
   }
 
@@ -106,6 +107,96 @@ class _HomeState extends State<Home> {
     return contentsRepository.loadContentsFromLocation(currentLocation);
   }
 
+  _makeDataList(List<Map<String, String>> datas){
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      //아이템을 만드는 위젯
+      itemBuilder: (BuildContext _content, int index) {
+        return Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              children: [
+                //이미지
+                ClipRRect(
+                  //모서리 둥글게
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  child: Image.asset(
+                    datas[index]["image"].toString(),
+                    width: 100,
+                    height: 100,
+                  ),
+                ),
+
+                //metaData
+                //Expanded를 해줌으로서 이미지가 사용하는 영역외에는 모두 사용하게 함
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    padding: const EdgeInsets.only(left: 20),
+                    //width: MediaQuery.of(context).size.width
+                    // device 사이즈만큼을 불러옴 -100은 이미지사이즈가 100이기떄문에.
+                    // 하지만 연산이 많아질 것임.
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          datas[index]['title'].toString(),
+                          //글자가 화면을 넘어가면 ...으로 변경됨. 미사용시 자동 줄바꿈
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          datas[index]['location'].toString(),
+                          //withOpacity : 연하게
+                          style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.3)),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          calcStringToWon(datas[index]['price'].toString()),
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/svg/heart_off.svg',
+                                  width: 13,
+                                  height: 13,
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                //좋아요이미지와 숫자사이의 간격을 위해 SizedBox를 넣음
+                                Text(datas[index]['likes'].toString()),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ));
+      },
+      //아이템 사이의 간격을 어떻게 구성할지, 지금은 라인이지만 Custom하도록 도와줌
+      separatorBuilder: (BuildContext _content, int index) {
+        return Container(height: 1, color: Colors.black.withOpacity(0.5));
+      },
+      itemCount: 10,
+    );
+  }
+
   Widget _bodyWidget() {
     //기다렸다 데이터가 처리될때까지 도와주는 builder
     return FutureBuilder(
@@ -118,99 +209,19 @@ class _HomeState extends State<Home> {
           return Center(child: CircularProgressIndicator());
         }
 
+        if(snapshot.hasError){
+          return Center(child: Text('데이터 오류'),);
+        }
 
-        List<Map<String, String>> datas = snapshot.data;
+        //데이터가 있는 경우
+        if(snapshot.hasData){
+          return _makeDataList(snapshot.data);
+        }
 
-        return ListView.separated(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          //아이템을 만드는 위젯
-          itemBuilder: (BuildContext _content, int index) {
-            return Container(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  children: [
-                    //이미지
-                    ClipRRect(
-                      //모서리 둥글게
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      child: Image.asset(
-                        datas[index]["image"].toString(),
-                        width: 100,
-                        height: 100,
-                      ),
-                    ),
-
-                    //metaData
-                    //Expanded를 해줌으로서 이미지가 사용하는 영역외에는 모두 사용하게 함
-                    Expanded(
-                      child: Container(
-                        height: 100,
-                        padding: const EdgeInsets.only(left: 20),
-                        //width: MediaQuery.of(context).size.width
-                        // device 사이즈만큼을 불러옴 -100은 이미지사이즈가 100이기떄문에.
-                        // 하지만 연산이 많아질 것임.
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              datas[index]['title'].toString(),
-                              //글자가 화면을 넘어가면 ...으로 변경됨. 미사용시 자동 줄바꿈
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              datas[index]['location'].toString(),
-                              //withOpacity : 연하게
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.black.withOpacity(0.3)),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text(
-                              calcStringToWon(datas[index]['price'].toString()),
-                              style: TextStyle(fontWeight: FontWeight.w500),
-                            ),
-                            Expanded(
-                              child: Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    SvgPicture.asset(
-                                      'assets/svg/heart_off.svg',
-                                      width: 13,
-                                      height: 13,
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    //좋아요이미지와 숫자사이의 간격을 위해 SizedBox를 넣음
-                                    Text(datas[index]['likes'].toString()),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ));
-          },
-          //아이템 사이의 간격을 어떻게 구성할지, 지금은 라인이지만 Custom하도록 도와줌
-          separatorBuilder: (BuildContext _content, int index) {
-            return Container(height: 1, color: Colors.black.withOpacity(0.5));
-          },
-          itemCount: 10,
-        );
+        //나머지
+        return Center(child: Text('해당 지역에 데이터가 없습니다.'),);
       },
     );
-
   }
 
   @override
