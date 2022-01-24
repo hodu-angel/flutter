670866b0f3f30edadd7749b3a780d99ed72b94c8
seohyoju <kinnylovemy@gmail.com>
2022-01-25 00:53:00 +0900

    Stack widget,Positioned add

--- carrot_market_sample/lib/page/detail.dart
+++ carrot_market_sample/lib/page/detail.dart
@@ -12,7 +12,7 @@ class DetailContentView extends StatefulWidget {
 
 class _DetailContentViewState extends State<DetailContentView> {
   Size? size;
-  List<String>? imgList;
+  List<Map<String, String>?>? imgList;
   int? _current;
   final CarouselController _controller = CarouselController();
 
@@ -22,11 +22,11 @@ class _DetailContentViewState extends State<DetailContentView> {
     size = MediaQuery.of(context).size;
     _current = 0;
     imgList = [
-      widget.data!['image'].toString(),
-      widget.data!['image'].toString(),
-      widget.data!['image'].toString(),
-      widget.data!['image'].toString(),
-      widget.data!['image'].toString(),
+      {'id' : '1', 'url' :  widget.data!['image'].toString()},
+      {'id' : '2', 'url' :  widget.data!['image'].toString()},
+      {'id' : '3', 'url' :  widget.data!['image'].toString()},
+      {'id' : '4', 'url' :  widget.data!['image'].toString()},
+      {'id' : '5', 'url' :  widget.data!['image'].toString()},
     ];
   }
 
@@ -61,7 +61,7 @@ class _DetailContentViewState extends State<DetailContentView> {
 
   Widget _bodyWidget() {
     return Container(
-      child: Column(
+      child: Stack(
         children: [
           Hero(
             tag: widget.data!['cid'].toString(),
@@ -81,9 +81,9 @@ class _DetailContentViewState extends State<DetailContentView> {
                 }
               ),
               //slide를 보여줄 이미지
-              items: imgList?.map((url) {
+              items: imgList?.map((map) {
                 return Image.asset(
-                  url,
+                  map!['url'] as String,
                   width: size!.width,
                   fit: BoxFit.fill,
                 );
@@ -91,24 +91,35 @@ class _DetailContentViewState extends State<DetailContentView> {
               carouselController: _controller,
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: imgList!.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => _controller.animateToPage(entry.key),
-                child: Container(
-                  width: 12.0,
-                  height: 12.0,
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white
-                          : Colors.black)
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-                ),
-              );
-            }).toList(),
+          Positioned(
+            //positioned는 영역을 크기만큼만 갖고있기때문에,부분적으로 위치설정한다
+            //left 0 right 0 이면 왼쪽끝부터 오른쪽 끝까지 사용한다는 것. 공간 확대
+            bottom: 0,
+            left: 0,
+            right: 0,
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList!.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => _controller.animateToPage(entry.key),
+                  child: Container(
+                    width: 12.0,
+                    height: 12.0,
+                    //horizontal로 이미지 슬라이드버튼 사이간격을 늘린다.
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 5.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                        //선택 됐을 때 색
+                            ? Colors.white
+                        //미선택일 때 색
+                            : Colors.white.withOpacity(0.5))
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                  ),
+                );
+              }).toList(),
+            ),
           ),
         ],
       ),
