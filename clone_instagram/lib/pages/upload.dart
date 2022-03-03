59682de4d684ae8892230c52c74d6a0d6dbfc11a
seohyoju <kinnylovemy@gmail.com>
2022-03-03 18:38:56 +0900

    uploadPage appBar, preview add

--- clone_instagram/lib/pages/upload.dart
+++ clone_instagram/lib/pages/upload.dart
@@ -1,10 +1,122 @@
+import 'package:clone_instagram/components/image_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+//내 device의 folder group을 그대로 불러들여서 뿌려주게 할 것임
 class Upload extends StatelessWidget {
   const Upload({Key? key}) : super(key: key);
 
+  Widget _imagePreview() {
+    return Container(
+      width: Get.width,
+      height: Get.width,
+      color: Colors.green,
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //앞 row
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: const [
+                Text(
+                  '갤러리',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: Color(0xff808080),
+                    borderRadius: BorderRadius.circular(30)),
+                child: Row(
+                  children: [
+                    ImageData(IconsPath.imageSelectIcon),
+                    const SizedBox(
+                      width: 7,
+                    ),
+                    const Text(
+                      '여러 항목 선택',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 5,),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xff808080)
+                ),
+                child: ImageData(IconsPath.cameraIcon),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.grey,);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(IconsPath.closeImage),
+          ),
+        ),
+        title: const Text(
+          'New Post',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.nextImage,
+                width: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _imagePreview(),
+            _header(),
+            // _imageSelectList(),
+          ],
+        ),
+      ),
+    );
   }
 }
