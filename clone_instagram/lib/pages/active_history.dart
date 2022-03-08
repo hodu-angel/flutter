2d09006e9ae2f3ce9fa99f349f0cd0d882a366ef
seohyoju <kinnylovemy@gmail.com>
2022-03-08 18:11:17 +0900

    Text.rich로 Text의 여러 스타일을 한번에 사용

--- clone_instagram/lib/pages/active_history.dart
+++ clone_instagram/lib/pages/active_history.dart
@@ -1,10 +1,104 @@
+import 'package:clone_instagram/components/avatar_widget.dart';
 import 'package:flutter/material.dart';
 
 class ActiveHistory extends StatelessWidget {
   const ActiveHistory({Key? key}) : super(key: key);
 
+  //아바타, 내용, 시간
+  Widget _activeitemOne() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          AvatarWidget(
+              type: AvatarType.TYPE2,
+              size: 40,
+              thumbPath:
+                  'https://i.pinimg.com/736x/3b/25/6f/3b256f387cb37cc831fa1386e214769a.jpg'),
+          const SizedBox(width: 10),
+          //글씨가 중간중간 진한부분도 있고 섞여있기 때문에 여러 문자스타일을 잡아주기 위해 사용
+          const Expanded(
+            child: Text.rich(
+              TextSpan(
+                text: 'hodu_angel',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+                children: [
+                  TextSpan(
+                    text: '님이 회원님의 게시물을 좋아합니다.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' 5 일전',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 13,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _newRecentlyActiveView(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          _activeitemOne(),
+          _activeitemOne(),
+          _activeitemOne(),
+          _activeitemOne(),
+          _activeitemOne(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          '활동',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _newRecentlyActiveView('오늘'), //오늘
+            _newRecentlyActiveView('이번주'), //이번주
+            _newRecentlyActiveView('이번달'), //이번달
+          ],
+        ),
+      ),
+    );
   }
 }
