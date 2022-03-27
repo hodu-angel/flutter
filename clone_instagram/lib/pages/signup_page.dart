9806ec327f762c3e8a8602418cff63f8ca5db319
seohyoju <kinnylovemy@gmail.com>
2022-03-27 23:48:24 +0900

    회원가입page_이미지변경버튼, 닉네임 받기 add

--- clone_instagram/lib/pages/signup_page.dart
+++ clone_instagram/lib/pages/signup_page.dart
@@ -17,22 +17,49 @@ class SignupPage extends StatelessWidget {
             ),
           ),
         ),
+        const SizedBox(height: 15),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('이미지 변경'),
+        ),
       ],
+    );
+  }
+
+  Widget _nickname() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(10),
+          hintText: '닉네임',
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         centerTitle: true,
-        title: const Text('회원가입'),
+        title: const Text(
+          '회원가입',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            const SizedBox(height: 30),
             _avatar(),
+            const SizedBox(height: 30),
+            _nickname(),
           ],
         ),
       ),
