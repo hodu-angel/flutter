5ec6180a91f776e8739743a93dd60c57cf1b064a
seohyoju <kinnylovemy@gmail.com>
2022-03-31 18:16:16 +0900

    Get.putAsync add

--- getx_example/lib/pages/dependencys/dependency_manage_page.dart
+++ getx_example/lib/pages/dependencys/dependency_manage_page.dart
@@ -40,8 +40,19 @@ class DependencyManagePage extends StatelessWidget {
               },
               child: const Text('Get.lazyPut'),
             ),
+
+            //putAsync는 put처럼 비슷하지만 페이지에 접근할 때
+            //비동기식으로 데이터를 받아오거나 가공처리를 하고나서
+            //controller를 instance화 할 때 사용된다.
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Get.to(const GetPut(), binding: BindingsBuilder(() {
+                  Get.putAsync<DependencyController>(() async {
+                    await Future.delayed(const Duration(seconds: 5));
+                    return DependencyController();
+                  });
+                }));
+              },
               //비동기를 처리해주고 put을 해준다.
               child: const Text('Get.putAsync'),
             ),
