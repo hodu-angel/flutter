f8285367db2a88ff6cc4a7c1d1d0822d47159a06
seohyoju <kinnylovemy@gmail.com>
2022-04-04 20:24:58 +0900

    new board upload page ui add

--- clone_instagram/lib/pages/upload/upload_description.dart
+++ clone_instagram/lib/pages/upload/upload_description.dart
@@ -1,12 +1,175 @@
+import 'package:clone_instagram/components/image_data.dart';
 import 'package:clone_instagram/controller/upload_controller.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class UploadDescription extends GetView<UploadController> {
   const UploadDescription({Key? key}) : super(key: key);
 
+  Widget _description() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 60,
+            height: 60,
+            child: Image.file(
+              controller.filteredImage!,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Expanded(
+            child: TextField(
+              //maxLines: null 여러줄 입력이 가능하다. 키보드에 엔터키가 활성화 된다.
+              maxLines: null,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 15),
+                hintText: '문구 입력...',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget infoOne(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 17,
+        ),
+      ),
+    );
+  }
+
+  //선 나누기
+  Widget get line => const Divider(
+        color: Colors.grey,
+      );
+
+  Widget snsInfo() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'facebook',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              Switch(value: false, onChanged: (bool value){})
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Twitter',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              Switch(value: false, onChanged: (bool value){})
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Tumblr',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              Switch(value: false, onChanged: (bool value){})
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: Get.back,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(
+              IconsPath.backBtnIcon,
+              width: 50,
+            ),
+          ),
+        ),
+        title: const Text(
+          '새 게시물',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.uploadComplete,
+                width: 50,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Stack(
+        //영역을 전체 확장하는 편법이다.
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: GestureDetector(
+              onTap: () {
+                //화면밖을 누르면 닫힌다.
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    _description(),
+                    line,
+                    infoOne('사람 태그하기'),
+                    line,
+                    infoOne('위치 추가'),
+                    line,
+                    infoOne('다른 미디어에도 게시'),
+                    snsInfo(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
